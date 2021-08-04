41f08fb63681bb949111e87d42cedc3b1327d1f6
Ivan Contreras <ivanc.contre@gmail.com>
2021-08-04 12:45:43 -0400

    Updates

---
+++ precache-manifest.c0b6785bb6976644af1ee20445d28c36.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f0601ca5296f862ee11b72e5b9a318e",
+    "url": "https://ivancontre.github.io/react-joutnalapp/index.html"
+  },
+  {
+    "revision": "e3056fd8c6e9569ce3de",
+    "url": "https://ivancontre.github.io/react-joutnalapp/static/css/2.2ce0d9b2.chunk.css"
+  },
+  {
+    "revision": "59fdd986582f2b06c8c9",
+    "url": "https://ivancontre.github.io/react-joutnalapp/static/css/main.f95fe01c.chunk.css"
+  },
+  {
+    "revision": "e3056fd8c6e9569ce3de",
+    "url": "https://ivancontre.github.io/react-joutnalapp/static/js/2.613a8b39.chunk.js"
+  },
+  {
+    "revision": "06ae1b980035305a30157a925629d1b7",
+    "url": "https://ivancontre.github.io/react-joutnalapp/static/js/2.613a8b39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59fdd986582f2b06c8c9",
+    "url": "https://ivancontre.github.io/react-joutnalapp/static/js/main.fd7ace09.chunk.js"
+  },
+  {
+    "revision": "69c974055fd67424939e",
+    "url": "https://ivancontre.github.io/react-joutnalapp/static/js/runtime-main.64357141.js"
+  }
+]);
